21daca07bc85247c08b7e005fa51368a56678bcf
sidawasthi420 <144908193+sidawasthi420@users.noreply.github.com>
2026-02-27 02:03:55 +0530

    Update browserHooks.java

--- src/test/java/hooks/browserHooks.java
+++ src/test/java/hooks/browserHooks.java
@@ -1,8 +1,14 @@
 package hooks;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import config.ConfigReader;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
@@ -11,11 +17,24 @@ public class browserHooks {
 	public static WebDriver driver = null;
 	
 	@Before
-	public void launchBrowser() throws InterruptedException
+	public void launchBrowser() throws InterruptedException, MalformedURLException
 	{
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		Thread.sleep(4000);
+		boolean seleniumGrid = Boolean.parseBoolean(ConfigReader.getProperty("seleniumGrid"));
+		String gridURL = ConfigReader.getProperty("gridURL");
+		
+		if (seleniumGrid) {
+	        // Use Selenium Grid
+	        DesiredCapabilities capabilities = new DesiredCapabilities();
+	        capabilities.setBrowserName("chrome");
+	        driver = new RemoteWebDriver(new URL(gridURL), capabilities);
+	    } else {
+	        // Use local ChromeDriver
+	        driver = new ChromeDriver();
+	    }
+
+	    driver.manage().window().maximize();
+	    Thread.sleep(4000);
+
 	}
 	
 	@After
